2e4ab5160c900ccf074f0cde36cffa15c3589ec9
Rohit Khot <rohit.r.khot@gmail.com>
2015-12-23 18:55:45 -0500

    Commit 2 - R Code with comments

--- cachematrix.R
+++ cachematrix.R
@@ -12,6 +12,9 @@ makeCacheMatrix <- function(x = matrix()) {
   
   matinv <- NULL
   set <- function(y) {
+## use of `<<-` to assign a value to an object in an environment 
+## which is different from the current environment
+    
     x <<- y           
     matinv <<- NULL
   }
@@ -31,13 +34,44 @@ makeCacheMatrix <- function(x = matrix()) {
 cacheSolve <- function(x, ...) {
   
   matinv <- x$getinverse()
+  
+## To check if the inverse has already been calculated
+  
   if(!is.null(matinv)) {
     message("getting cached data.")
     return(matinv)
   }
+  
+## else, calculate the inverse
   data <- x$get()
   matinv <- solve(data)
+  
+## To set the value of the inverse in the cache via the setinv function.
   x$setinverse(matinv)
-  matinv                        ## Returns a matrix that is the inverse of 'x'
+  
+## Return a matrix that is the inverse of 'x'
+  matinv                        
 
 }
+
+## Testing the functions :
+## > x = matrix(1:4, nrow=2, ncol=2)
+## > m = makeCacheMatrix(x)
+## > m$get()
+##       [,1]  [,2]
+## [1,]    1    3
+## [2,]    2    4
+
+## First run without the cache
+## > cacheSolve(m)
+##       [,1]  [,2]
+## [1,]   -2    1.5
+## [2,]    1   -0.5
+
+## Second run when function retrives from the cache
+## > cacheSolve(m)
+## getting cached data.
+##       [,1]  [,2]
+## [1,]   -2    1.5
+## [2,]    1   -0.5
+## 
